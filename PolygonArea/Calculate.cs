da354ad14e72f1df785e570d060460fed1dfd310
farshad21 <34832220+farshad21@users.noreply.github.com>
2024-09-01 18:06:03 +0330

    Making Calculate class

---
+++ PolygonArea/Calculate.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolygonArea
+{
+    class Calculate
+    {
+        public static double CalculatePolygonArea(double[,] vertices, int numVertices)
+        {
+            double area = 0;
+
+            // Shoelace formula
+            for (int i = 0; i < numVertices - 1; i++)
+            {
+                area += vertices[i, 0] * vertices[i + 1, 1];
+                area -= vertices[i + 1, 0] * vertices[i, 1];
+            }
+
+            // Add the last vertex back to the first vertex
+            area += vertices[numVertices - 1, 0] * vertices[0, 1];
+            area -= vertices[0, 0] * vertices[numVertices - 1, 1];
+
+            area = Math.Abs(area) / 2.0;
+
+            return area;
+
+        }
+    }
+}
